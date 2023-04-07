856a3b57040bbf08516c4f1ef31bb3730bebf3ea
PavlushaSource <pavlushkin.dima1785@gmail.com>
2023-04-07 03:40:48 +0300

    feat: Fix protected open field in BST

--- BST.kt
+++ BST.kt
@@ -1,7 +1,7 @@
 open class BinarySearchTree<K: Comparable<K>>() {
-    private var left: BinarySearchTree<K>? = null
-    private var right: BinarySearchTree<K>? = null
-    private var parent: BinarySearchTree<K>? = null
+    protected open var left: BinarySearchTree<K>? = null
+    protected open var right: BinarySearchTree<K>? = null
+    protected open var parent: BinarySearchTree<K>? = null
     protected open var key: K? = null
     protected open var value: Any? = null
 }
